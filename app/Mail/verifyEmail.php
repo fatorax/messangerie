f924140044c5ae1c98e7c33d33fbd7f52b738060
Romain Bellini <rorobellini@gmail.com>
2026-01-03 06:33:10 +0100

    fix: rename VerifyEmail class (case sensitive on Linux)

--- app/Mail/verifyEmail.php
+++ app/Mail/verifyEmail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class verifyEmail extends Mailable
+class VerifyEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
